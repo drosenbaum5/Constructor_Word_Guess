298236d691a30d840e4a05b47002f9f884255a11
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-09-23 14:16:16 -0400

    Added login index.js file.

--- letter.js
+++ letter.js
@@ -1,24 +1,26 @@
-function letter(value) {
+
+function Letter(value) {
     this.letter = value;
     this.guessed = false;
-
+  
     this.toString = function() {
-        if(this.letter === " ") {
-            this.guessed = true;
-            return " ";
+      if (this.letter === " ") {
+        this.guessed = true;
+        return " ";
+      } else {
+        if (this.guessed === false) {
+          return "_";
         } else {
-            if(this.guessed === false) {
-                return "_";
-            } else {
-                return this.letter;
-            }
+          return this.letter;
         }
-    }
-    this.guess = function (guess) {
-        if(guess === this.letter) {
-            this.guessed = true;
-        }
-    }
-}
-
-module.exports = Letter;
+      }
+    };
+  
+    this.guess = function(guess) {
+      if (guess === this.letter) {
+        this.guessed = true;
+      }
+    };
+  }
+  
+  module.exports = Letter;
--- word.js
+++ word.js
@@ -1,23 +1,28 @@
-var Letter = require("./letter");
+  
+var Letter = require("./letter.js");
 
 function Word(answer) {
-    this.objArray = [];
+  //Letter objects array
+  this.objArray = [];
 
-    for(var i = 0; i < answer.length; i++) {
-        var letter = new Letter(answer[i]);
-        this.objArray.push(letter);
-    }
+  for (var i = 0; i < answer.length; i++) {
+    var letter = new Letter(answer[i]);
+    this.objArray.push(letter);
+  }
 
-    this.log = function () {
-        var answerLog = "";
-        for(var i = 0; i <this.objArray.length; i++) {
-            answerLog += this.objArray[i] = " ";
-        }
-        console.log(answerLog + "\n=============================\n");
+  this.log = function() {
+    answerLog = "";
+    for (var i = 0; i < this.objArray.length; i++) {
+      answerLog += this.objArray[i] + " ";
     }
-        this.userGuess = function(input){
-            for(var i = 0; i < this.objArray.length; i++) {
-                this.objArray[i].guess(input);
-            }
-        }
+    console.log(answerLog + "\n========================\n");
+  };
+
+  this.userGuess = function(input) {
+    for (var i = 0; i < this.objArray.length; i++) {
+      this.objArray[i].guess(input);
     }
+  };
+}
+
+module.exports = Word;
